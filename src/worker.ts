8dea98dcb9a5ea49349075cf2a27188b1db5391f
zhangsan <1009102963@qq.com>
2026-06-19 08:01:14 +0800

    同时支持 timeout 和 operationTimeout、支持 followRedirects 参数
    // 同时支持 timeout 和 operationTimeout
    const timeout = parseNumberParam(
        requestUrl.searchParams.get('timeout') || 
        requestUrl.searchParams.get('operationTimeout')
    );
    
    // 支持 followRedirects 参数
    const followRedirects = parseBooleanParam(requestUrl.searchParams.get('followRedirects'));
    
    Signed-off-by: zhangsan <1009102963@qq.com>

--- src/worker.ts
+++ src/worker.ts
@@ -110,7 +110,9 @@ export default {
 			const lang = requestUrl.searchParams.get('lang') || undefined;
 			
 			// Parse new optional parameters
-			const timeout = parseNumberParam(requestUrl.searchParams.get('timeout'));
+			const timeout = parseNumberParam(requestUrl.searchParams.get('timeout') || requestUrl.searchParams.get('operationTimeout');
+			// 支持 followRedirects 参数
+      const followRedirects = parseBooleanParam(requestUrl.searchParams.get('followRedirects'));
 			const contentLengthLimit = parseNumberParam(requestUrl.searchParams.get('contentLengthLimit'));
 			const contentLengthRequired = parseBooleanParam(requestUrl.searchParams.get('contentLengthRequired'));
 			const userAgent = requestUrl.searchParams.get('userAgent') || undefined;
